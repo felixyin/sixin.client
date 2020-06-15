13becb7fcb0e890fc8920439b56492876c4b96dd
尹彬 <1052921694@qq.com>
2020-06-15 09:59:45 +0800

    Initial commit

---
+++ src/config.js
@@ -0,0 +1,21 @@
+/**
+ * <plusmancn@gmail.com> created at 2017
+ *
+ * Copyright (c) 2017 plusmancn, all rights
+ * reserved.
+ *
+ * @flow
+ *
+ * 用户配置信息
+ */
+
+const config = {
+    'local': {
+        server: 'http://192.168.0.134:7078'
+    },
+    'dev': {
+        server: 'http://im-server.plusman.cn'
+    }
+};
+
+export default config.local;
